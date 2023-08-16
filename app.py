de6af20be6d9869dc63b5769343b0e5ec089cad0
Stanislav Leleka <stanislav.leleka@gmail.com>
2023-08-16 20:38:59 +0300

    retrain

--- app.py
+++ app.py
@@ -22,13 +22,37 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    liked_movies = data.get('liked_movies', [])
-    movie_id = data.get('movie_id', 0)
+    liked_movie_ids = data.get('liked_movie_ids', [])
+    movie_ids = data.get('movie_ids', [])
 
-    # Call the prediction method
-    predictions = prediction.predict_user_preference_extended(
-        liked_movies, movie_id)
+    predictions = []
+
+    for movie_id in movie_ids:
+        prediction_result = prediction.predict_user_preference_extended(
+            liked_movie_ids, movie_id)
+        predictions.append(
+            {
+                "movie_id": movie_id,
+                "tfidf": prediction_result[0],
+                "cosine": prediction_result[1],
+            }
+        )
+
     return jsonify(predictions)
+
+
+@app.route('/retrain', methods=['POST'])
+def retrain():
+    try:
+        training = MovieRecommenderTraining(mongodb_connection_string)
+        training.train("models/model.pkl")
+
+        global prediction
+        prediction.load_model()
+
+        return jsonify({"status": "success", "message": "Model retrained successfully"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 
 if __name__ == '__main__':
